447fc7ee63a830d42c5be6c948a13e4fed353849
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-01-19 17:49:53 +0900

    로그인 뷰 작성

---
+++ js/login.js
@@ -0,0 +1,26 @@
+document.querySelector("#login-id-submit").addEventListener("click", function () {
+    let email = document.querySelector("#email");
+    let password = document.querySelector("#password");
+    let loginUrl = "http://localhost:8080/api/users/login";
+    let data = { email: email.value, password: password.value };
+
+    axios.post(loginUrl, data, null).then(response => {
+        console.log(response);
+
+        if(response.status == 200) {
+            console.log(response.headers["authorization"]);
+            localStorage.setItem("authorization", response.headers["authorization"]);
+        }
+    
+    }).catch(function () {
+        alert("일치하는 유저가 없습니다. 다시 시도해주세요 ");
+        password.value = "";
+    });
+
+}, false);
+
+// 토큰을 가져온 뒤 로컬 스토리지에 저장예정
+// axios에서 보안 헤더가 안나오는 문제 있음. 아마 axios에 설정을 해줘야하거나 서버에서 허용을 해줘야할듯.
+// 네트워크 탭에는 제대로 나옴.
+
+
