3f26a2abdbecce7295640aa7b0242f223eb8cc99
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-01-23 19:36:00 +0900

    리다이렉션 경로 오타 수정

--- js/signup.js
+++ js/signup.js
@@ -16,23 +16,17 @@ document.getElementById("submit-btn").addEventListener("click", function (e) {
   const response = signup(data);
 
 });
-// TODO 회원가입에 실패했을 경우 예외처리 구현 필요.
-// 일단 회원가입 처리는 잘 됨.
-
-/*
-  username": "T",
-  "email": "string",
-  "password": "534IDT53",
-  "confirmPassword": "string"
-*/
 
 async function signup(data) {
   let url = "https://hobbyback.store/api/users/signup";
   const response = await axios.post(url, data).then(response => {
       if(response.status == 200) {
-        window.location.href = "/iogin.html";
+        window.location.href = "/login.html";
       }
-  })
+  }).catch(e => {
+    alert("오류가 발생했습니다");
+    window.location.href = "/login.html";
+  });
   return response;
 
 }
