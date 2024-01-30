f6b2c8dc4aeb4c5b527e8e3a3f629ba6a19e0b7b
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-01-30 15:53:25 +0900

    게시글 조회 로그아웃 가능

--- js/postRead.js
+++ js/postRead.js
@@ -98,3 +98,25 @@ document.querySelector(".my-info").addEventListener("click", function (evt) {
   const accordion = document.querySelector(".accordion");
   accordion.classList.toggle("close");
 })
+
+document.querySelector(".logout").addEventListener("click", function () {
+  logout().then(response => {
+    if (response.status == 200) {
+      localStorage.removeItem("authorization");
+      window.location.href = "/index.html";
+    }
+  }).catch(e => {
+
+  });
+});
+
+async function logout() {
+  let url = `https://hobbyback.store/api/users/logout`;
+  let response = await axios.post(url, null, {
+    headers: {
+      "authorization": localStorage.getItem("authorization")
+    }
+  });
+
+  return response;
+}
