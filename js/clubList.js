caccd41f25907ba0ebc930e3993103f2a4ce9258
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-02-03 18:37:10 +0900

    최상단 이동 버튼 추가

--- js/clubList.js
+++ js/clubList.js
@@ -109,7 +109,7 @@ document.querySelector('.keyword').addEventListener('keyup', function (event) {
 const toTopEl = document.querySelector('#to-top')
 
 window.addEventListener('scroll', _.throttle(function () {
-  if (window.scrollY > 500) {
+  if (window.scrollY > 400) {
     gsap.to(toTopEl, .2, {
       x: 0
     })
